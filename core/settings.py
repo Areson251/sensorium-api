0e3ed0332f4b42d05352d4f9c34cea2fa40e6a63
sersh4nt <39828832+sersh4nt@users.noreply.github.com>
2023-11-22 11:55:34 +0300

    Revert "add models to control"

--- core/settings.py
+++ core/settings.py
@@ -8,20 +8,19 @@ SECRET_KEY = "django-insecure-kl!z7^uk87ileuuuv*7s57(&8@0vp24rf=%mbw5&4-e#o_1#az
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
-    "django.contrib.contenttypes",  
+    "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_spectacular",
     "drf_spectacular_sidecar",
-    "control",
 ]
 
 MIDDLEWARE = [
